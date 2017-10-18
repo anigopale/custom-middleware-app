ade12827b73e86bf357f608ac7621b47eb2a4f25
Aniruddh Gopale <anigopale@gmail.com>
2017-10-18 14:04:21 +0530

    return action data from reducer, render data

--- src/components/user_list.js
+++ src/components/user_list.js
@@ -11,8 +11,8 @@ class UserList extends Component {
     return (
       <div className="card card-block">
         <h4 className="card-title">{user.name}</h4>
-        <p className="card-text">Cheese Factory</p>
-        <a className="btn btn-primary">Email</a>
+        <p className="card-text">{user.company.name}</p>
+        <a className="btn btn-primary" href={user.website}>Website</a>
       </div>
     );
   }
@@ -27,6 +27,7 @@ class UserList extends Component {
 }
 
 function mapStateToProps(state){
+  console.log(state);
   return {users: state.users };
 }
 
